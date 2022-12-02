4d2d7d7d5e1fa22c18f08da52c8aa9791b8c4d51
“saurabh” <saurabhsrivastav.312@gmail.com>
2022-12-02 11:33:04 +0530

    my changes

---
+++ component/portfolio/data.js
@@ -0,0 +1,55 @@
+
+import IMG1 from '../../assests/portfolio1.jpg';
+import IMG2 from '../../assests/portfolio2.jpg';
+import IMG3 from '../../assests/portfolio3.jpg';
+import IMG4 from '../../assests/portfolio4.jpg';
+import IMG5 from '../../assests/portfolio5.png';
+import IMG6 from '../../assests/portfolio6.jpg';
+
+
+
+export const data= [ 
+    {
+    id: 1,
+    title: "This is Portfolio Title",
+    img: IMG1,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    {
+    id: 2,
+    title: "This is Portfolio Title",
+    img: IMG2,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    {
+    id: 3,
+    title: "This is Portfolio Title",
+    img: IMG3,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    {
+    id: 4,
+    title: "This is Portfolio Title",
+    img: IMG4,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    {
+    id: 5,
+    title: "This is Portfolio Title",
+    img: IMG5,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    {
+    id: 6,
+    title: "This is Portfolio Title",
+    img: IMG6,
+    Github: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    demo: "https://www.linkedin.com/in/shreyansh-shukla-38842b187",
+    },
+    
+  ]
