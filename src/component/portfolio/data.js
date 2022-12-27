2743653b8caead19f101a74c7d0c5708fe32870c
Saurabh <saurabhsrivastav.312@gmail.com>
2022-12-28 03:29:16 +0530

    projects

--- src/component/portfolio/data.js
+++ src/component/portfolio/data.js
@@ -11,45 +11,32 @@ import IMG6 from '../../assests/portfolio6.jpg';
 export const data= [ 
     {
     id: 1,
-    title: "This is Portfolio Title",
+    title: "Movie App",
     img: IMG1,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
+    Github: "https://github.com/57saurabh/Movie-suggestion-app/",
+    demo: "https://precious-paprenjak-1cf58f.netlify.app/",
     },
     {
     id: 2,
-    title: "This is Portfolio Title",
+    title: "Portfolio Website",
     img: IMG2,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
+    Github: "https://github.com/57saurabh/my-portfolio",
+    demo: "https://saurabh-srivastava.vercel.app/",
     },
     {
     id: 3,
-    title: "This is Portfolio Title",
+    title: "Weather App",
     img: IMG3,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
+    Github: "https://github.com/57saurabh/weather-app-react-API-",
+    demo: "https://weatherappweb.vercel.app/",
     },
     {
     id: 4,
-    title: "This is Portfolio Title",
+    title: "Salaah.org.in",
     img: IMG4,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
+    Github: "https://salaah.org.in",
+    demo: "https://salaah.org.in",
     },
-    {
-    id: 5,
-    title: "This is Portfolio Title",
-    img: IMG5,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
-    },
-    {
-    id: 6,
-    title: "This is Portfolio Title",
-    img: IMG6,
-    Github: "https://github.com/57saurabh/",
-    demo: "https://github.com/57saurabh/",
-    },
+   
     
   ]
